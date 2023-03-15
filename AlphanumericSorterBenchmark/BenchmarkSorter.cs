539fdb2def93b193a77c6b8fa83c2b0ce68db0b6
Tigran Karapetyan <RWBV73@motorolasolutions.com>
2023-03-16 00:30:38 +0100

    1. Generator added to the Benchmark tests as well 2. 100GB file generation and Sorting was tested

--- AlphanumericSorterBenchmark/BenchmarkSorter.cs
+++ AlphanumericSorterBenchmark/BenchmarkSorter.cs
@@ -6,22 +6,56 @@ namespace AlphanumericSorterBenchmark
     [MemoryDiagnoser()]
     public class BenchmarkSorter : IDisposable
     {
-        private const string InputFileName = @"C:\AltiumTestProject\TempStorage\1GB.txt";
-        private const string OutputFileName = @"C:\AltiumTestProject\TempStorage\1GB_OUT.txt";
+        private static readonly double TestFileRelativeSize = 100;
+        private static readonly FileSizeExtensions.MetricPrefixes MetricPrefix = FileSizeExtensions.MetricPrefixes.Giga;
 
+        private readonly long InputFileSize;
+        private readonly string InputFileName;
+        private readonly string OutputFileName;
+
+        private long generatedLinesCount;
         private AlphanumericSorterHelper? sorterHelper;
 
         public BenchmarkSorter()
         {
-            Initialize();
+            var storageDirectory = Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData),
+                "AltiumTestProject",
+                "TempStorage");
+
+            if (!Directory.Exists(storageDirectory))
+            {
+                Directory.CreateDirectory(storageDirectory);
+            }
+
+            InputFileSize = TestFileRelativeSize.GetFileSizeInBytes(MetricPrefix);
+            var formattedFileSize = ((double)InputFileSize).FormatFileSize();
+
+            InputFileName = Path.Combine(storageDirectory, $"{formattedFileSize}_Benchmark_Test.txt");
+            OutputFileName = Path.Combine(storageDirectory, $"{formattedFileSize}_Benchmark_Test_OUT.txt");
+
+            // Generate benchmark test file
+            GenerateTestFile();
+
+            // Sorting
+            InitializeSorter();
             Sort();
             SaveToFile();
         }
 
         [Benchmark]
-        public void Initialize()
+        public void GenerateTestFile()
         {
-            sorterHelper = new AlphanumericSorterHelper(InputFileName);
+            generatedLinesCount = RandomFileGenerator.GenerateTestFile(InputFileName, InputFileSize);
+        }
+
+        [Benchmark]
+        public void InitializeSorter()
+        {
+            if (generatedLinesCount > 0)
+            {
+                sorterHelper = new AlphanumericSorterHelper(InputFileName);
+            }
         }
 
         [Benchmark]
