1279c99cb88ba61d8554f70ae0cd3d12e1d287ff
Tigran Karapetyan <RWBV73@motorolasolutions.com>
2023-03-15 16:37:18 +0100

    Adding temporary console app for test purposes using BenchmarkDotNet NuGet

---
+++ AlphanumericSorterBenchmark/BenchmarkSorter.cs
@@ -0,0 +1,48 @@
+﻿using BenchmarkDotNet.Attributes;
+using MultiSorterLib;
+
+namespace AlphanumericSorterBenchmark
+{
+    [MemoryDiagnoser()]
+    public class BenchmarkSorter : IDisposable
+    {
+        private const string InputFileName = @"C:\AltiumTestProject\TempStorage\1GB.txt";
+        private const string OutputFileName = @"C:\AltiumTestProject\TempStorage\1GB_OUT.txt";
+
+        private AlphanumericSorterHelper? sorterHelper;
+
+        public BenchmarkSorter()
+        {
+            Initialize();
+            Sort();
+            SaveToFile();
+        }
+
+        [Benchmark]
+        public void Initialize()
+        {
+            sorterHelper = AlphanumericSorterHelper.LoadFromFile(InputFileName);
+        }
+
+        [Benchmark]
+        public void Sort()
+        {
+            sorterHelper?.Sort();
+        }
+
+        [Benchmark]
+        public void SaveToFile()
+        {
+            sorterHelper?.SaveToFile(OutputFileName);
+        }
+
+        public void Dispose()
+        {
+            sorterHelper?.Dispose();
+            sorterHelper = null;
+
+            GC.Collect();
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ AlphanumericSorterBenchmark/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics;
+using AlphanumericSorterBenchmark;
+using BenchmarkDotNet.Running;
+
+Stopwatch sw = new();
+
+do
+{
+    Console.WriteLine("Starting file Loading and Sorting... ");
+
+    sw.Reset();
+    sw.Start();
+
+    try
+    {
+        BenchmarkRunner.Run<BenchmarkSorter>();
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine("An exception was thrown during app execution.");
+        Console.WriteLine($"Error details: {e}");
+    }
+    finally
+    {
+        GC.Collect();
+        GC.WaitForPendingFinalizers();
+    }
+
+    sw.Stop();
+
+    Console.WriteLine("The file was successfully Sorted and Saved!");
+    Console.WriteLine($"OVERALL TIME: {sw.Elapsed:c}");
+} while (Console.ReadKey().Key != ConsoleKey.Escape);
