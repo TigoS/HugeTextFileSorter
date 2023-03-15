74eee7e6dcdea0d896162a626a703121f106225a
Tigran Karapetyan <RWBV73@motorolasolutions.com>
2023-03-15 16:20:38 +0100

    1. New class library project MultiSorterLib added to the solution 2. .gitignore updated

---
+++ MultiSorterLib/AlphanumericEntity.cs
@@ -0,0 +1,44 @@
+﻿namespace MultiSorterLib
+{
+    public class AlphanumericEntity : IComparable<AlphanumericEntity>, IEquatable<AlphanumericEntity>
+    {
+        public const char Delimiter = '.';
+        public const string LinePattern = "{0}{1} {2}";
+        
+        public AlphanumericEntity(string stringPart, int numericPart)
+        {
+            StringPart = stringPart;
+            NumericPart = numericPart;
+        }
+
+        public string StringPart { get; }
+
+        public int NumericPart { get; }
+
+        public string EntityLine => string.Format(LinePattern, NumericPart, Delimiter, StringPart);
+        
+        public int CompareTo(AlphanumericEntity? other)
+        {
+            if (other is null)
+            {
+                return -1;
+            }
+
+            int strComparison = string.Compare(StringPart, other.StringPart, StringComparison.Ordinal);
+
+            return strComparison != 0 ? strComparison : NumericPart.CompareTo(other.NumericPart);
+        }
+
+        public bool Equals(AlphanumericEntity? other)
+        {
+            return other is not null &&
+                   NumericPart.Equals(other.NumericPart) &&
+                   (StringPart.Equals(other.StringPart));
+        }
+        
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(StringPart, NumericPart);
+        }
+    }
+}
---
+++ MultiSorterLib/AlphanumericSorterHelper.cs
@@ -0,0 +1,72 @@
+﻿namespace MultiSorterLib
+{
+    public sealed class AlphanumericSorterHelper
+    {
+        public AlphanumericSorterHelper()
+        {
+            Entities = Enumerable.Empty<AlphanumericEntity>();
+        }
+
+        public AlphanumericSorterHelper(IEnumerable<string> lines)
+        {
+            Entities = EnumerateEntities(lines);
+        }
+
+        public IEnumerable<AlphanumericEntity> Entities;
+
+        public long EntitiesCount => Entities.LongCount();
+
+        public static AlphanumericSorterHelper LoadFromFile(string fileName)
+        {
+            return File.Exists(fileName) ? new AlphanumericSorterHelper(File.ReadLines(fileName)) : new AlphanumericSorterHelper();
+        }
+        
+        public void Sort()
+        {
+            var entityArray = Entities.ToArray();
+            Array.Sort(entityArray);
+        }
+
+        public void SaveToFile(string fileName)
+        {
+            if (EntitiesCount > 0)
+            {
+                var directory = Path.GetDirectoryName(fileName);
+                if (!Directory.Exists(directory))
+                {
+                    Directory.CreateDirectory(directory);
+                }
+
+                File.WriteAllLines(fileName, Entities.Select(s => s.EntityLine));
+            }
+        }
+
+        public void CleanupEntities()
+        {
+            Entities.GetEnumerator().Dispose();
+            Entities = Enumerable.Empty<AlphanumericEntity>();
+        }
+
+        private static IEnumerable<AlphanumericEntity> EnumerateEntities(IEnumerable<string> lines)
+        {
+            IEnumerator<string> enumerator = lines.GetEnumerator();
+
+            try
+            {
+                while (enumerator.MoveNext())
+                {
+                    var parts = enumerator.Current.Split(AlphanumericEntity.Delimiter);
+
+                    if (parts.Length == 2 && int.TryParse(parts[0].Trim(), out int numericPart))
+                    {
+                        yield return new AlphanumericEntity(parts[1].Trim(), numericPart);
+                    }
+                }
+            }
+            finally
+            {
+                enumerator.Dispose();
+            }
+        }
+    }
+}
