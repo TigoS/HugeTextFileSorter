7d3a4c71fc23f867f790b7cc67486772e9cfbab9
TigOS <Tigran.A.Karapetyan@gmail.com>
2026-02-05 21:01:06 +0100

    Adding external sorter

---
+++ MultiSorterLib/ExternalSorter.cs
@@ -0,0 +1,48 @@
+﻿namespace MultiSorterLib
+{
+    public static class ExternalSorter
+    {
+        const int CHUNK_SIZE_MB = 256;
+        const long CHUNK_SIZE_MULTIPLIER = 1024L;
+
+        public static List<string> CreateSortedChunks(string inputFile)
+        {
+            
+            var tempFiles = new List<string>();
+            long maxBytes = CHUNK_SIZE_MB * CHUNK_SIZE_MULTIPLIER * CHUNK_SIZE_MULTIPLIER;
+
+            using var reader = new StreamReader(inputFile);
+            if (reader != null)
+            {
+                List<string> lines;
+                long currentBytes;
+                string line, tempFile;
+
+                while (!reader.EndOfStream)
+                {
+                    lines = new List<string>();
+                    currentBytes = 0;
+
+                    while (!reader.EndOfStream && currentBytes < maxBytes)
+                    {
+                        line = reader.ReadLine() ?? string.Empty;
+
+                        if (!string.IsNullOrWhiteSpace(line))
+                        {
+                            lines.Add(line);
+                            currentBytes += line.Length * sizeof(char);
+                        }
+                    }
+
+                    lines.Sort(StringComparer.Ordinal);
+
+                    tempFile = Path.GetTempFileName();
+                    File.WriteAllLines(tempFile, lines);
+                    tempFiles.Add(tempFile);
+                }
+            }
+
+            return tempFiles;
+        }
+    }
+}
